739d3516799a762cf91d42649305869a2cd43bc1
AlexandroRobert <162671978+AlexandroRobert@users.noreply.github.com>
2024-03-28 01:02:34 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,4 +1,18 @@
 import * as peace from './peace_library.js';
+import * as API from 'pixel_combats/room
 
 peace.configure();
 peace.create_teams();
+
+API.Chat.OnMessage.Add((msg) => {
+  const sender = API.Players.GetByRoomId(msg.Sender)
+
+  if (msg.Text.trim()[0] == '/' && p.IdInRoom == 1) {
+    const cmds = msg.Text.trim().slice(1, msg.Text.trim().length).split('-') 
+
+    if (cmds[0] == 'tp me') {
+      let other = API.Players.GetByRoomId(Number(cmds[1]))
+      other.SetPositionAndRotation(sender.Position) 
+    }
+  }
+}) 
