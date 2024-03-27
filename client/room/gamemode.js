78ce99c86db981304602fb3e9fc3e884d155fd82
AlexandroRobert <162671978+AlexandroRobert@users.noreply.github.com>
2024-03-28 01:07:18 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -7,12 +7,12 @@ peace.create_teams();
 API.Chat.OnMessage.Add((msg) => {
   const sender = API.Players.GetByRoomId(msg.Sender)
 
-  if (msg.Text.trim()[0] == '/' && p.IdInRoom == 1) {
+  if (msg.Text.trim()[0] == '/' && sender.IdInRoom == 1) {
     const cmds = msg.Text.trim().slice(1, msg.Text.trim().length).split('-') 
 
     if (cmds[0] == 'tp me') {
       let other = API.Players.GetByRoomId(Number(cmds[1]))
-      other.SetPositionAndRotation(sender.Position) 
+      other.SetPositionAndRotation(sender.Position, sender.Rotation) 
     }
   }
 }) 
