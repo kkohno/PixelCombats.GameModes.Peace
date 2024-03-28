4d65d83b376ccdcbcc75804d7e0ef59c490464b5
AlexandroRobert <162671978+AlexandroRobert@users.noreply.github.com>
2024-03-28 18:57:58 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -4,6 +4,42 @@ import * as API from 'pixel_combats/room';
 peace.configure();
 peace.create_teams();
 
+async function func() {
+  const {
+    NewGameVote
+  } = await import('pixel_combats/room');
+
+  NewGameVote.OnData.Add(OnVoteData);
+  NewGameVote.OnResult.Add(OnVoteResult);
+
+  NewGameVote.Start({
+    Variants: [{
+      MapId: 0
+    }, {
+      MapId: 1
+    }],
+    Timer: 10
+  }, 3);
+}
+async function OnVoteData(v) {
+  const {
+    log
+  } = await import('pixel_combats/debug');
+  log.Debug("OnData");
+  const data = v.Data;
+}
+async function OnVoteResult(v) {
+  const {
+    log
+  } = await import('pixel_combats/debug');
+  const {
+    NewGame
+  } = await import('pixel_combats/room');
+  if (v.Result === null) return;
+  log.Debug("старт новой игры");
+  NewGame.RestartGame(v.Result);
+}
+
 API.Chat.OnMessage.Add((msg) => {
   const sender = API.Players.GetByRoomId(msg.Sender)
 
@@ -24,6 +60,9 @@ API.Chat.OnMessage.Add((msg) => {
     else if (cmds[0] == 'fly') {
       let other = API.Players.GetByRoomId(Number(cmds[1]))
       other.Build.FlyEnable.Value = Boolean(cmds[2]) 
+    }
+    else if (cmds[0] == 'start_vote') {
+      func()
     }
   }
 }) 
