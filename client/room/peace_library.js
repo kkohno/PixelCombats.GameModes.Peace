8fa926b0504ed18b108740422f909abb51092625
nikushaMercy <145331393+nikushaMercy@users.noreply.github.com>
2025-01-29 01:09:45 +0900

    Update peace_library.js

--- client/room/peace_library.js
+++ client/room/peace_library.js
@@ -1,4 +1,4 @@
-import * as room from 'pixel_combats/room';
+/*import * as room from 'pixel_combats/room';
 import * as teams from './default_teams.js';
 
 // разрешает все что можно для строительства
@@ -85,3 +85,84 @@ export function create_teams() {
     // если игрок сменил команду или выбрал ее, то происходит спавн игрока
     room.Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn(); });
 }
+*/
+
+import * as room from 'pixel_combats/room';
+import * as teams from './default_teams.js';
+
+function set_inventory() {
+    const context = room.Inventory.GetContext();
+    context.Main.Value = false;
+    context.Secondary.Value = false;
+    context.Melee.Value = false;
+    context.Explosive.Value = false;
+    context.Build.Value = false;
+}
+
+function set_build_settings() {
+  const context = room.Build.GetContext();
+  Object.assign(context, {
+    Pipette: { Value: true },
+    BalkLenChange: { Value: true },
+    SetSkyEnable: { Value: true },
+    GenMapEnable: { Value: true },
+    ChangeCameraPointsEnable: { Value: true },
+    QuadChangeEnable: { Value: true },
+    BuildModeEnable: { Value: true },
+    CollapseChangeEnable: { Value: true },
+    RenameMapEnable: { Value: true },
+    ChangeMapAuthorsEnable: { Value: true },
+    LoadMapEnable: { Value: true },
+    ChangeSpawnsEnable: { Value: true },
+    BlocksSet: { Value: room.BuildBlocksSet.AllClear },
+  });
+}
+
+export function apply_room_options() {
+    const params = room.GameMode.Parameters;
+    const buildContext = room.Build.GetContext();
+    Object.assign(buildContext, {
+        FloodFill: { Value: params.GetBool("FloodFill") },
+        FillQuad: { Value: params.GetBool("FillQuad") },
+        RemoveQuad: { Value: params.GetBool("RemoveQuad") },
+        FlyEnable: { Value: params.GetBool("Fly") }
+    });
+
+    const damageContext = room.Damage.GetContext();
+    damageContext.DamageOut.Value = params.GetBool("Damage");
+    room.BreackGraph.OnlyPlayerBlocksDmg = params.GetBool("PartialDesruction");
+    room.BreackGraph.WeakBlocks = params.GetBool("LoosenBlocks");
+}
+
+
+export function configure() {
+    Object.assign(room.Properties.GetContext(), { GameModeName: { Value: "GameModes/Peace" } });
+    Object.assign(room.Ui.GetContext(), { Hint: { Value: "Hint/BuildBase" }, QuadsCount: { Value: true } });
+
+    room.BreackGraph.BreackAll = true;
+    room.Spawns.GetContext().RespawnTime.Value = 0;
+    set_build_settings();
+    set_inventory();
+    apply_room_options();
+}
+
+export function create_teams() {
+    const params = room.GameMode.Parameters;
+    const hasRedTeam = params.GetBool("RedTeam");
+    const hasBlueTeam = params.GetBool("BlueTeam");
+    const blueHasNothing = params.GetBool("BlueHasNothing");
+
+    if (hasRedTeam || (!hasRedTeam && !hasBlueTeam)) teams.create_team_red();
+    if (hasBlueTeam || (!hasRedTeam && !hasBlueTeam)) teams.create_team_blue();
+
+    room.Teams.OnAddTeam.Add(team => {
+        const isBlue = team.Id === teams.BLUE_TEAM_NAME;
+        const hasItems = !blueHasNothing;
+        team.Inventory.Melee.Value = isBlue ? hasItems : true;
+        team.Inventory.Build.Value = isBlue ? hasItems : true;
+        team.Inventory.BuildInfinity.Value = isBlue ? hasItems : true;
+    });
+
+    room.Teams.OnRequestJoinTeam.Add((player, team) => team.Add(player));
+    room.Teams.OnPlayerChangeTeam.Add(player => player.Spawns.Spawn());
+};
